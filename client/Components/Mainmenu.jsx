b8aae216a3aabffe2ebf000bf517e51711eff05f
Jon Ascencio <jonascencio1@gmail.com>
2020-10-31 20:40:58 -0700

    frontend boilerplate set up

--- client/Components/Mainmenu.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// export default Mainmenu extends Component {
-//     return (
-//         <div>
-//         <div />
-//     )
-// }
-
---
+++ client/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+// import all reducers here
+import TasksReducers from './TasksReducers';
+
+export default reducers;
