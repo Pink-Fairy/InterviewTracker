0967f8b559108860b5489c4d0ca647d8bc031942
Tania <tanya.o.lind@gmail.com>
2020-11-02 14:50:15 -0800

    style, tasks crud fixed

--- client/Components/TaskListHolder.jsx
+++ client/Components/TaskListHolder.jsx
@@ -3,60 +3,33 @@ import axios from 'axios';
 // import{ useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 // import useTodoState from './useTodoState';
-
 import TasksList from './TasksList.jsx';
 import TaskForm from './TaskForm.jsx';
-
-
 // useEffect - allows us to provide functionality that responds to certain data or functions in our code
   // takes in two parameters 
   // 1. function aka the effect
   // 2. the dependency array - param that determines if the effect gets fired or not. if one or more
       // variables in the array changes, then the effect changes, if it empty, the effect will be fired with the component is initially rendered  
-
 const TaskListHolder = () => {
-
   // set an initial state: either get all tasks from mongo or set it to an empty array
-
   // const initialState = localStorage.getItem('tasks') || [];
   const [tasks, setTasks] = useState([]);
   const [token, setToken] = useState('');
-
-
   const getTask = async(token) => {
     const response = await axios.get('/api/tasks', {
       headers: {Authorization: token}
     })
     setTasks(response.data);
   };
-   
-
     useEffect(() => {
       const token = localStorage.getItem('tokenStore');
       setToken(token);
-      // axios.get('/api/tasks', {
-      //   headers: {Authorization: token}
-      // })   
-      // .then(res => { 
-      //   console.log(res.data);
-      //   setTasks(res.data);
-      // })
-      // .catch(err => {
-      //   console.log(err);
-      // })
       if (token) {
         getTask(token)
       }
-      
     }, [tasks]);
-  
-
-
-
-
-
   const deleteTask = (taskMongoID) => {
-    // console.log(taskMongoID);
+    console.log(taskMongoID);
     const token = localStorage.getItem('tokenStore');
     setToken(token);
     axios.delete(`/api/tasks/${taskMongoID}`, {
@@ -70,35 +43,24 @@ const TaskListHolder = () => {
       );
     })
     .catch(err => console.log(err));
-   
   };
-
-  const updateTask = (task) => {
+  const updateTask = (task, id) => {
+    console.log('we are in the tasklist holder')
     console.log(task); // on click shows entire task obj
-
-    // const token = localStorage.getItem('tokenStore');
-    // setToken(token);
-    // axios.put(`/api/tasks/${taskMongoID}`, {
-    //   headers: {Authorization: token}
-    // })
-    // .then(res => {
-    //   // console.log(res);
-    //   const newTasks = tasks.filter((task) => task._id !== taskMongoID);
-    //   setTasks(
-    //     newTasks
-    //   );
-    // })
-    // .catch(err => console.log(err));
-   
+    console.log(id);
+    const token = localStorage.getItem('tokenStore');
+    setToken(token);
+    axios.put(`/api/tasks/${id}`, task, {
+      headers: {Authorization: token}
+    })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
   }
-
-
   return (
     <div className="tasks_container">
-      <Typography component="h1" variant="h2">
-        Tasks
-      </Typography>
-
+     
       <TaskForm getTask={getTask}/>
       <TasksList 
       tasks={tasks} 
@@ -108,5 +70,4 @@ const TaskListHolder = () => {
     </div>
   )
 }
-
 export default TaskListHolder;
--- client/Components/TasksList.jsx
+++ client/Components/TasksList.jsx
@@ -7,69 +7,95 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
-
-
-
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 const TasksList = ({tasks, deleteTask, updateTask }) => {
-
-
-
+  const [taskId, setTaskId] = useState('');
   const [isEditing, setIsEditing] = useState(false);
-  const [updatedTask, setUpdatedTask] = useState();
-
-
+  const [updatedTask, setUpdatedTask] = useState({
+    title: '', // text describing the task
+    date: '', // text describing the task
+  });
   const updateTaskState = (e) => {
+    const { name, value } = e.target
     setUpdatedTask({
-      ...tasks,
-      title: e.target.value,
-      date: e.target.value
+      ...updatedTask,
+      [name]: value
     });
-
-
   };
-
   const updateAndReset = (input, e) => {
     e.preventDefault();
-
-    
-    updateTask(input);
+    updateTask(input, taskId);
     setIsEditing(false);
+    setUpdatedTask({
+      title: '',
+      date: ''
+    });
+    setTaskId('');
   };
-
-
-
-
-
-
+  const style = {
+    visibility: isEditing ? "visible" : "hidden",
+    opacity: isEditing ? 1 : 0
+}
   return(
 <List>
     {tasks.map((task) => (
       <ListItem key={task._id} dense button>
-        <Checkbox tabIndex={-1} disableRipple />
-        <ListItemText primary={task.title} />
-        <ListItemText primary={new Date(task.date).toLocaleDateString()} />
-        <ListItemSecondaryAction>
-          <IconButton
-            aria-label="Delete"
-            onClick={() => {
-              deleteTask(task._id);
-            }}
-          >
-            <DeleteIcon />
-          </IconButton>
-          <IconButton
-            aria-label="Update"
-            onClick={() => {
-              updateTask(task);
-            }}
-          > 
-            <EditIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
+        <div className='task-title'>
+          <ListItemText primary={task.title} />
+        </div>
+        
+        <div className='task-date'>
+          <ListItemText  primary={new Date(task.date).toLocaleDateString()} />
+        </div>
+      
+      <ListItemSecondaryAction>
+        <IconButton
+          aria-label="Delete"
+          onClick={() => {
+            deleteTask(task._id);
+          }}
+        >
+          <DeleteIcon />
+        </IconButton>
+        <IconButton
+          aria-label="Update"
+          onClick={() => {
+            {console.log(task._id)}
+            setTaskId(task._id);
+            setIsEditing(true);
+            // updateTask(task);
+          }}
+        > 
+          <EditIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
     ))}
+<div style={style}>
+
+<form onSubmit={e => updateAndReset(updatedTask, e)} className="task-form"> 
+<TextField 
+variant="outlined"
+margin="normal"
+name='title'
+value={updatedTask.title}
+placeholder='Task'
+required
+onChange={updateTaskState}
+/>
+<TextField 
+variant="outlined"
+margin="normal"
+name='date'
+value={updatedTask.date}
+placeholder='Date'
+onChange={updateTaskState}
+/>
+<button type='submit' />
+</form>
+</div> 
   </List>
   )
 }
-
 export default TasksList;
