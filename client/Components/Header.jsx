22a2e16fe713b4b18ce1c3ba044ac893a62a562d
Tania <tanya.o.lind@gmail.com>
2020-11-01 18:36:32 -0800

    logout, styling login, joblists

---
+++ client/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+export default function Header({ setIsLogin }) {
+    
+    const logoutSubmit = () => {
+        localStorage.clear();
+        setIsLogin(false);
+    }
+    return (
+   
+        <div className="Header">
+                <Link to='/' onClick={logoutSubmit} className="logout">LOGOUT</Link>
+        </div>
+        
+    )
+}
